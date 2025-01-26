8c24a1822ca2f1d1a8b048c5c9c180a0a3693446
jhoso105106 <hjun105106@hotmail.co.jp>
2025-01-26 10:59:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -53,12 +53,12 @@ st.markdown("""
 """)
 
 # 画像ファイルのアップロード
-uploaded_files = st.file_uploader("画像ファイルをアップロードしてください", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+#uploaded_files = st.file_uploader("画像ファイルをアップロードしてください", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
 
-if uploaded_files:
-    for uploaded_file in uploaded_files:
+#if uploaded_files:
+#    for uploaded_file in uploaded_files:
         # 画像を保存
-        img = Image.open(uploaded_file)
-        img.save(os.path.join(save_dir, uploaded_file.name))
-        st.image(img, caption=f"アップロードされた画像: {uploaded_file.name}", use_column_width=True)
-        st.success(f"画像が保存されました: {uploaded_file.name}")
+#        img = Image.open(uploaded_file)
+#        img.save(os.path.join(save_dir, uploaded_file.name))
+#        st.image(img, caption=f"アップロードされた画像: {uploaded_file.name}", use_column_width=True)
+#        st.success(f"画像が保存されました: {uploaded_file.name}")
